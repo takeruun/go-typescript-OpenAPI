072bc62f35362ce487c28e447a77c6881ab10230
takeru.hashimoto <hstake25@gmail.com>
2022-07-28 09:42:19 +0900

    codegen echo 使用

--- main.go
+++ main.go
@@ -1,22 +1,35 @@
 package main
 
 import (
-	"net/http"
+	api "app/api"
+	controller "app/controller"
+	"fmt"
+	"os"
 
+	echomiddleware "github.com/deepmap/oapi-codegen/pkg/middleware"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	swagger, err := api.GetSwagger()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
+		os.Exit(1)
+	}
+	swagger.Servers = nil
+
 	// Echo instance
 	e := echo.New()
 
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(echomiddleware.OapiRequestValidator(swagger))
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	mainHandler := controller.NewMainHandler()
+
+	api.RegisterHandlers(e, mainHandler)
+
 	e.Logger.Fatal(e.Start(":3000"))
 }
