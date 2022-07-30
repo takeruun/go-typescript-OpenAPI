e97a2e8e4175b2a633acc02a81319019997704bd
takeru.hashimoto <hstake25@gmail.com>
2022-07-31 01:47:18 +0900

    front react

---
+++ front/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Sample Open API schema APP Go, TypeScript </p>
+      </header>
+      <div className="container">
+        <div className='buttons'>
+          <div>
+            <button>Hello</button>
+          </div>
+          <div>
+            <button>GoodMorning</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
